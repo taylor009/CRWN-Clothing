6bdf23cca62f258b6f3346d0d7e677cef505803f
taylor <taylor@baremetalcloud.com>
2020-04-27 17:23:50 -0400

    Created collection-preview component and also created shop page component.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import {Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => (
-    <div>
-        <h1>HATS PAGE</h1>
-    </div>
-)
+import ShopPage from "./pages/shop/shop.component";
 
 
 function App() {
@@ -16,7 +11,7 @@ function App() {
     <div>
       <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route exact path='/shop/hats' component={HatsPage} />
+          <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
