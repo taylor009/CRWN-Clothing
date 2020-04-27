4e820073e2332ab04eaf6c0a265e2826dbe9db85
taylor <taylor@baremetalcloud.com>
2020-04-27 19:06:33 -0400

    Created reuseable form input field and styled form.

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import FormInput from "../form-input/form-input.component";
+
+import './sign-in.styles.scss';
+
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        this.setState({email: '', password: ''})
+    }
+
+    handleChange = event => {
+        const { value, name } = event.target;
+
+        this.setState({[name]: value})
+    }
+
+    render() {
+        return(
+            <div className='sign-in'>
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput type="email" name='email' label='email' handleChange={this.handleChange} value={this.state.email} required/>
+
+                    <FormInput type="password" name='password' label='password' handleChange={this.handleChange} value={this.state.password} required/>
+
+                    <input type="submit" value="Submit Form"/>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn
