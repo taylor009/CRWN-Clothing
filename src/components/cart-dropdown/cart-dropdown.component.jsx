81e4556e2b393649a9be21ab663a406f05328285
taylor <taylor@baremetalcloud.com>
2020-04-29 13:54:47 -0400

    Improved cart dropdown user flow.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,10 +6,11 @@ import { withRouter } from 'react-router-dom';
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
 import { selectCartItems } from "../../redux/cart/cart.selectors";
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
 import './cart-dropdown.styles.scss';
 
-const CartDropDown = ({cartItems, history }) => (
+const CartDropDown = ({cartItems, history, dispatch }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             {
@@ -21,7 +22,7 @@ const CartDropDown = ({cartItems, history }) => (
                  <span className='empty-message'>Your cart is empty</span>
             }
         </div>
-        <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={() => {history.push('/checkout'); dispatch(toggleCartHidden());}}>GO TO CHECKOUT</CustomButton>
     </div>
 );
 
