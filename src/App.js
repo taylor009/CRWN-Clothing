ee2d1af6fb42d8a4ca4539dbd6d03408767f8315
taylor <taylor@baremetalcloud.com>
2020-04-29 13:25:28 -0400

    Created checkout page component and wired up the route inside of the react router.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import './App.css';
 
-import Header from "./components/header/header.component";
+
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
+import CheckoutPage from "./pages/checkout/checkout.component";
+
+import Header from "./components/header/header.component";
+
 import {auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from "./redux/user/user.selector";
@@ -46,6 +50,7 @@ class App extends React.Component {
               <Switch>
                   <Route exact path='/' component={HomePage} />
                   <Route path='/shop' component={ShopPage} />
+                  <Route exact path='/checkout' component={CheckoutPage} />
                   <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage/>)} />
               </Switch>
           </div>
