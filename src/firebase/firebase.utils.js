cd149354db800723964ae597555aa390ca97abbb
taylor <taylor@affair.dev>
2020-05-04 14:38:42 -0400

    Moved directory state to redux and converted directory component back to a functional component.

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,50 @@
+import firebase from "firebase/app";
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyBGVGzmHtr6dv-ZxB8-t2QUcTs8nuin7oE",
+    authDomain: "crwn-db-2477c.firebaseapp.com",
+    databaseURL: "https://crwn-db-2477c.firebaseio.com",
+    projectId: "crwn-db-2477c",
+    storageBucket: "crwn-db-2477c.appspot.com",
+    messagingSenderId: "669355568606",
+    appId: "1:669355568606:web:7f43b4bc505fa9b283c14f",
+    measurementId: "G-XKYYT4GZ2X"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+   if (!userAuth) return
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+   if(!snapShot.exists) {
+       const { displayName, email } = userAuth;
+       const createdAt = new Date();
+
+       try {
+           await userRef.set({
+               displayName,
+               email,
+               createdAt,
+               ...additionalData
+           })
+       } catch(error) {
+           console.log(`error creating user ${error.message}`);
+       }
+   }
+   return userRef;
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
