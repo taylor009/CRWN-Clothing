8b1676e011151c9bbd23f6b9c7bf6c8037bb28d3
taylor <taylor@affair.dev>
2020-05-04 19:44:03 -0400

    Moved our directory and shop data into it's own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from "reselect";
+import { Route } from 'react-router-dom';
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-import { selectCollections } from "../../redux/shop/shop.selectors";
+import './shop.styles.scss';
 
-const ShopPage = ({collections}) => {
-    return (<div className='shop-page'>
-          {collections.map(({id, ...otherCollectionProps}) => (<CollectionPreview key={id} {...otherCollectionProps} />))}
-      </div>)
-};
+const ShopPage = ({ match }) => (
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
-const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
-});
 
-export default connect(mapStateToProps)(ShopPage);
+
+export default ShopPage;
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,11 +1,11 @@
-import SHOP_DATA from "./shop.data";
+import SHOP_DATA from './shop.data';
 
-const INITAL_STATE = {
-	collections : SHOP_DATA
+const INITIAL_STATE = {
+	collections: SHOP_DATA
 };
 
-const shopReducer = (state = INITAL_STATE, action) => {
-	switch(action.type) {
+const shopReducer = (state = INITIAL_STATE, action) => {
+	switch (action.type) {
 		default:
 			return state;
 	}
