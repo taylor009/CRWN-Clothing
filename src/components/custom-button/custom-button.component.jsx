bbfcedb2176f2a4cfb772f82e46c845097dab3d2
taylor <taylor@affair.dev>
2020-05-05 16:43:34 -0400

    added styled components to application.

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,12 +1,9 @@
-import React from "react";
+import React from 'react';
 
-import './custom-button.styles.scss';
+import { CustomButtonContainer } from './custom-button.styles';
 
-
-const CustomButton = ({ children, isGoogleSignIn, inverted,  ...otherProps }) => (
-    <button className={`${inverted ? 'inverted': ''} custom-button`} {...otherProps}>
-        {children}
-    </button>
+const CustomButton = ({ children, ...props }) => (
+  <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
 );
 
 export default CustomButton;
