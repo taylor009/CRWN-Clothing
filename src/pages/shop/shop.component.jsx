1e0a3975f59211a75b8c3e86b25253e0dbaae1a2
taylor <taylor@affair.dev>
2020-05-07 15:41:58 -0400

    converted shop.data call to promise patteren.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -26,7 +26,7 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
-    this.unsubscribeFromSnapShot = collectionRef.onSnapshot(async snapshot => {
+    collectionRef.get().then(snapshot => {
      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
      updateCollections(collectionsMap);
      this.setState({ loading: false });
