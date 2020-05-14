2e0b146cc3b778d3e72bf34bf5a5becbf18e2a5a
taylor <taylor@affair.dev>
2020-05-14 13:38:14 -0400

    production redux store.

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,27 +1,21 @@
-import { createStore, applyMiddleware } from "redux";
-import { persistStore } from "redux-persist";
+import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import createSagaMiddleware from 'redux-saga';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './root-reducer';
-import rootSaga from "./root-saga";
 
-const sagaMiddleware = createSagaMiddleware();
+const middlewares = [logger];
 
-const middlewares = [sagaMiddleware];
-
-if(process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development') {
 	middlewares.push(logger);
 }
 
-export const store = createStore(
-	rootReducer, composeWithDevTools(
-		applyMiddleware(...middlewares)
-	));
-
-sagaMiddleware.run(rootSaga);
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
+
+export default { store, persistStore };
+
+
 
 
